4db8e2ef8598a5e020af0162e5825ad8fc04cb63
Code Lao <109976097+codelao@users.noreply.github.com>
2023-02-13 20:53:05 +0300

    1.0.1 (read CHANGELOG)

--- code/db.py
+++ code/db.py
@@ -4,7 +4,8 @@ import sqlite3
 class Database:
     def __init__(self, db_file):
         self.con = sqlite3.connect(db_file, check_same_thread=False)
-
+        self.con.cursor().execute("CREATE TABLE IF NOT EXISTS data(username TEXT PRIMARY KEY, password TEXT, join_date TEXT)")
+        
     def add_userdata(self, username, password, join_date):
         with self.con:
             self.con.cursor().execute("INSERT INTO data VALUES (?, ?, ?)", (username, password, join_date,))
